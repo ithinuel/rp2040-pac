16a5a49bbadfccbf3d3c6c88b78b834089772140
Wilfried Chauveau <wilfried.chauveau@ithinuel.me>
2023-07-23 08:38:57 +0100

    --wip-- [skip ci]

--- update.sh
+++ update.sh
@@ -2,10 +2,10 @@
 
 set -ex
 
-#cargo install --version 0.21.0 svd2rust
-cargo install --version 0.8.0  form
+cargo install --version 0.28.0 svd2rust
+cargo install --version 0.10.0 form
+#cargo install --version 0.3.0  svdtools
 rustup component add rustfmt
-#pip3 install --upgrade --user "svdtools==0.1.23"
 
 rm -rf src
 mkdir src
